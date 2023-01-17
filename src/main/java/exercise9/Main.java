8d4625e4f23bec6eef188af63afe5d1b5b27888b
EugeneKuzneczov <kuzneczov_ea@mail.ru>
2023-01-17 13:13:36 +0300

    Задание 9

---
+++ src/main/java/exercise9/Main.java
@@ -0,0 +1,7 @@
+package exercise9;
+
+public class Main {
+    public static void main(String[] args) {
+        new StudentWithSystemTime(new Student()).learn();
+    }
+}
---
+++ src/main/java/exercise9/Student.java
@@ -0,0 +1,13 @@
+package exercise9;
+
+import java.util.stream.*;
+
+public class Student implements Learner {
+
+    @Override
+    public void learn() {
+        System.out.println("Я учусь. .zZ");
+        IntStream.range(0, 1000000).forEach(i -> Math.log(i));
+        System.out.println("Я закончил учиться");
+    }
+}
